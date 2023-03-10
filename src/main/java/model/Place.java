f334cea66ca3f49cd8dd00232ecbb1b13d215e12
DomiMartinGlogi <93612122+DomiMartinGlogi@users.noreply.github.com>
2023-03-10 13:57:23 +0100

    created Place class, test commit

---
+++ src/main/java/model/Place.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Place {
+    private String name;
+
+    public Place(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}

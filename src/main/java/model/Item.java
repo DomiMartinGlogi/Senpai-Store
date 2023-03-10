cdb47dec8255f57f8df07eb1266dacd43f5b887e
DomiMartinGlogi <93612122+DomiMartinGlogi@users.noreply.github.com>
2023-03-10 15:04:53 +0100

    Added completely untested Code for abstract class Item and Storage class.

--- src/main/java/model/Item.java
+++ src/main/java/model/Item.java
@@ -1,9 +1,42 @@
 package model;
 
+import java.time.Instant;
+import java.util.Date;
+
 public abstract class Item {
     private String name;
+    private boolean checkedOut;
+    private Date checkOutDate;
 
     public Item(String name){
         this.name = name;
+        checkedOut = false;
+        checkOutDate = null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean checkOutStatus() {
+        return checkedOut;
+    }
+
+    public void checkOut(){
+        if (checkedOut){
+            System.out.println(name + " is already checked out.");
+        } else {
+            checkedOut = true;
+            checkOutDate = Date.from(Instant.now());
+        }
+    }
+
+    public void returnIt() {
+        if (checkOutDate == null || !checkedOut){
+            System.out.println(name + " cannot be returned, it is already here.");
+        } else {
+            checkedOut = false;
+            checkOutDate = null;
+        }
     }
 }
--- src/main/java/model/Storage.java
+++ src/main/java/model/Storage.java
@@ -6,10 +6,69 @@ import java.util.List;
 public class Storage {
     private List<Item> contents;
     private String name;
+    int capacity;
 
     public Storage(Item item, String name, int capacity){
         contents = new ArrayList<>(capacity);
         this.name = name;
+        this.capacity = capacity;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public List<Item> getContents() {
+        return contents;
+    }
+
+    public boolean addItem(Item item){
+        if (contents.size() == capacity) {
+            return false;
+        }
+        contents.add(item);
+        return true;
+    }
+
+    public boolean removeItem(Item item){
+        if (!contents.contains(item)){
+            return false;
+        }
+        contents.remove(item);
+        return true;
+    }
+
+    public List<Item> searchForItem(String name){
+        List<Item> returnList = new ArrayList<>();
+        for (Item i:contents){
+            returnList.add(i);
+        }
+        return returnList;
+    }
+
+    public boolean checkOutItem(Item item){
+        if (!contents.contains(item)){
+            return false;
+        } else {
+            Item inList = contents.get(contents.indexOf(item));
+            if (!inList.checkOutStatus()){
+                return false;
+            }
+            inList.checkOut();
+            return true;
+        }
+    }
+
+    public boolean returnItem(Item item){
+        if (!contents.contains(item)){
+            return false;
+        } else {
+            Item inList = contents.get(contents.indexOf(item));
+            if (inList.checkOutStatus()){
+                inList.returnIt();
+                return true;
+            }
+            return false;
+        }
+    }
 }

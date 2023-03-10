99453e272fc4cfc6d5f658eb7747c1c5a9aabbf6
DomiMartinGlogi <93612122+DomiMartinGlogi@users.noreply.github.com>
2023-03-10 15:25:53 +0100

    Added Documentation, fixed minor thought mistake.

--- src/main/java/model/Item.java
+++ src/main/java/model/Item.java
@@ -3,11 +3,21 @@ package model;
 import java.time.Instant;
 import java.util.Date;
 
+/**
+ * Models the Basic Structure and utility of Items should not be instantiated
+ * If generic class is needed just extend it.
+ * @author Dominik Martin Glogowski
+ */
 public abstract class Item {
     private String name;
     private boolean checkedOut;
     private Date checkOutDate;
 
+    /**
+     * Creates the Item
+     * Sets checkedOut to false and checkOutDate to null.
+     * @param name
+     */
     public Item(String name){
         this.name = name;
         checkedOut = false;
@@ -18,10 +28,17 @@ public abstract class Item {
         return name;
     }
 
+    /**
+     * Checks whether the item is checked out or not, effectively a getter in all but name.
+     * @return false when the item is present, true if it is checked out.
+     */
     public boolean checkOutStatus() {
         return checkedOut;
     }
 
+    /**
+     * Checks out the item, sets checkedOut to true, and the checkOutDate to Instant.Now()
+     */
     public void checkOut(){
         if (checkedOut){
             System.out.println(name + " is already checked out.");
@@ -31,6 +48,9 @@ public abstract class Item {
         }
     }
 
+    /**
+     * Returns the Item, checkedOut will be set to false, checkOutDate is set back to null.
+     */
     public void returnIt() {
         if (checkOutDate == null || !checkedOut){
             System.out.println(name + " cannot be returned, it is already here.");
--- src/main/java/model/Storage.java
+++ src/main/java/model/Storage.java
@@ -3,12 +3,25 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Models the lowest level of Storage, ie a box or a level of a shelf
+ * @author Dominik Martin Glogowski
+ */
 public class Storage {
     private List<Item> contents;
     private String name;
     int capacity;
 
-    public Storage(Item item, String name, int capacity){
+    /**
+     * Basic ctor
+     * Generates an empty ArrayList for contents with set capacity.
+     * @param name gives the name of the storage, like "Shelf A-1"
+     * @param capacity Maximum NUMBER of items in the list. Use -1 for effectively infinite.
+     */
+    public Storage(String name, int capacity){
+        if (capacity == -1){
+            capacity = Integer.MAX_VALUE;
+        }
         contents = new ArrayList<>(capacity);
         this.name = name;
         this.capacity = capacity;
@@ -22,6 +35,12 @@ public class Storage {
         return contents;
     }
 
+    /**
+     * Adds an Item to contents.
+     * Checks for space in contents.
+     * @param item
+     * @return False if capacity at maximum, True if item successfully added.
+     */
     public boolean addItem(Item item){
         if (contents.size() == capacity) {
             return false;
@@ -30,6 +49,11 @@ public class Storage {
         return true;
     }
 
+    /**
+     * Attempts to remove the given item.
+     * @param item
+     * @return False if item not in contents, false if successfully removed.
+     */
     public boolean removeItem(Item item){
         if (!contents.contains(item)){
             return false;
@@ -38,14 +62,26 @@ public class Storage {
         return true;
     }
 
-    public List<Item> searchForItem(String name){
+    /**
+     * Searches for any Item where the name contains the given String filter.
+     * @param filter
+     * @return ArrayList of Item
+     */
+    public List<Item> searchForItem(String filter){
         List<Item> returnList = new ArrayList<>();
         for (Item i:contents){
-            returnList.add(i);
+            if (i.getName().contains(filter)){
+                returnList.add(i);
+            }
         }
         return returnList;
     }
 
+    /**
+     * Checks out the given item.
+     * @param item
+     * @return False if the item is already checked out or not in contents. True if successfully checked out.
+     */
     public boolean checkOutItem(Item item){
         if (!contents.contains(item)){
             return false;
@@ -59,6 +95,11 @@ public class Storage {
         }
     }
 
+    /**
+     * Attempts to return a given Item
+     * @param item
+     * @return False if item not in list or not checked out, True if successfully returned.
+     */
     public boolean returnItem(Item item){
         if (!contents.contains(item)){
             return false;
